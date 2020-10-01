8f7f7af8f7ae9dd957d964ff83a235371a0af616
shengjunjie <1228473270@qq.com>
2020-10-01 21:48:31 +0800

    学习文本

--- basic-code/Login/src/main/java/Web/servlet.java
+++ basic-code/Login/src/main/java/Web/servlet.java
@@ -3,6 +3,7 @@ package Web;
 
 import Dao.UserDao;
 import domain.User;
+import org.apache.commons.beanutils.BeanUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,6 +11,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
 
 @WebServlet("/loginServlet")
 public class servlet extends HttpServlet {
@@ -18,12 +21,23 @@ public class servlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         req.setCharacterEncoding("utf-8");
-        String username=req.getParameter("username");
+       /* String username=req.getParameter("username");
         String password=req.getParameter("password");
 
         User loginUser=new User();
         loginUser.setUsername(username);
-        loginUser.setPassword(password);
+        loginUser.setPassword(password);*/
+        Map<String, String[]> map = req.getParameterMap();
+        User loginUser=new User();
+
+        try {
+            BeanUtils.populate(loginUser,map);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
 
         UserDao dao=new UserDao();
         User user = dao.login(loginUser);
--- basic-code/tomcat/src/main/java/ServletTest.java
+++ basic-code/tomcat/src/main/java/ServletTest.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet("/Servlet")
-public class Servlet extends HttpServlet {
+public class ServletTest extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
