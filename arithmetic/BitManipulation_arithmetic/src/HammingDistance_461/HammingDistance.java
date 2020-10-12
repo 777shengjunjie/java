643cb01b45f60903f8cae4d6fcf8f71ef3bd1c5c
Sjj <1228473270@qq.com>
2020-10-12 11:55:13 +0800

    算法461

---
+++ arithmetic/BitManipulation_arithmetic/src/HammingDistance_461/HammingDistance.java
@@ -0,0 +1,23 @@
+package HammingDistance_461;
+
+
+
+/*
+两个整数之间的汉明距离指的是这两个数字对应二进制位不同的位置的数目。
+
+        给出两个整数 x 和 y，计算它们之间的汉明距离。
+
+        注意：
+        0 ≤ x, y < 231.
+*/
+public class HammingDistance {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int hammingDistance(int x,int y){
+
+
+    }
+}
